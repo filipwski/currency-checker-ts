c7025ec84455b48f83af0b5149b7e2815d0a44aa
FilipWSki <filipwski@gmail.com>
2019-10-13 22:07:17 +0200

    Structure fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './test_objects/View/App';
+import App from './View/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './test_objects/View/App';
+import App from './View/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
